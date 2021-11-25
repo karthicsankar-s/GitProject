c1ff56c87abd299d054f70e52f2d20f8dec3f378
karthic <ks.test4950@gmail.com>
2021-11-25 07:59:10 +0530

    develop branch

--- src/DateClass.java
+++ src/DateClass.java
@@ -20,6 +20,7 @@ public class DateClass {
 	{
 		System.out.println("getData");
 		System.out.println("getData2");
+		System.out.println("Changes for new develop branch");
 	}
 	
 	public static void get() 
